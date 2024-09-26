adf5683ede02824a33c8b2a5d2cb23116627503f
Dani Gámez Franco <gmzcodes@outlook.com>
2024-09-26 18:35:26 +0200

    Add __getServerInfo() to playground and add NPM scripts to link or install othent easily.

--- scripts/sync-version.sh
+++ scripts/sync-version.sh
@@ -1,11 +1,18 @@
 packageJSON="./package.json"
 
 if [ ! -f $packageJSON ]; then
-    echo -e "\033[0;31mCoverage summary not found at \"$packageJSON\"!"
+    echo -e "\033[0;31mpackage.json not found at \"$packageJSON\"!"
     exit 1
 fi
 
 othentVersion=$(node -p "require('$packageJSON').dependencies['@othent/kms'].replace('^', '')")
+originalOthentVersion=$othentVersion
+
+if [[ $originalOthentVersion == link:* ]]; then
+  echo -e "\033[0;32m✔\033[0m Reinstalling latest `@othent/kms` version..."
+  pnpm install-othent
+  othentVersion=$(node -p "require('$packageJSON').dependencies['@othent/kms'].replace('^', '')")
+fi
 
 if [ -z "$othentVersion" ]; then
     echo -e "\033[0;31mCould not read `@othent/kms` version!"
@@ -19,3 +26,8 @@ sed -i -e "s/\"version\": \".*\"/\"version\": \"${othentVersion}\"/" ./package.j
 # Note there's no g flag used in the regular expression to make sure only one line is affected (just in case).
 
 git add ./package.json
+
+if [[ $originalOthentVersion == link:* ]]; then
+  echo -e "\033[0;32m✔\033[0m Restoring the linked `@othent/kms` previously in use..."
+  pnpm link-othent
+fi
